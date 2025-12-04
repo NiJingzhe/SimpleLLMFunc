117411b8a843eb33b983d4dd8eb7110c755e65a3
倪旌哲 <1448976235@qq.com>
2025-12-04 18:05:20 +0800

    feat(type): 新增decorator和multimodal类型支持模块
    - 新增type/decorator.py：装饰器相关的类型定义
    - 新增type/multimodal.py：多模态类型支持
    - 更新type/__init__.py导出新模块

--- SimpleLLMFunc/type/__init__.py
+++ SimpleLLMFunc/type/__init__.py
@@ -1,9 +1,16 @@
-from SimpleLLMFunc.llm_decorator.multimodal_types import Text, ImgUrl, ImgPath
+# 多模态类型
+from SimpleLLMFunc.type.multimodal import ImgPath, ImgUrl, Text
+
+# 接口类型
 from SimpleLLMFunc.interface.llm_interface import LLM_Interface
+
+# 装饰器相关类型
+from SimpleLLMFunc.type.decorator import HistoryList
 
 __all__ = [
     "Text",
     "ImgUrl",
     "ImgPath",
     "LLM_Interface",
+    "HistoryList",
 ]
---
+++ SimpleLLMFunc/type/decorator.py
@@ -0,0 +1,7 @@
+"""Decorator-related type definitions."""
+
+from typing import Any, Dict, List
+
+# Type alias for chat history
+HistoryList = List[Dict[str, Any]]
+
